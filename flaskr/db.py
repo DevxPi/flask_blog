28f5e037dbf635f4545d3b40510405c22795d9f2
DevxPi <macro.devxpi@gmail.com>
2023-05-19 21:20:59 +0800

    (app): register command init-db to application

--- flaskr/db.py
+++ flaskr/db.py
@@ -19,7 +19,7 @@ def close_db(e=None):
         db.close()
 
 def init_db():
-    db = get_db
+    db = get_db()
 
     with current_app.open_resource('schema.sql') as f:
         db.executescript(f.read().decode('utf8'))
@@ -30,3 +30,8 @@ def init_db_command():
     """ Clear the existing data and create new tables """
     init_db()
     click.echo('Initialized the database.')
+
+# Register the command with the application
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
