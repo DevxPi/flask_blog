7e0740bc9d6401df9165cc8b49e83baa9c72d26a
DevxPi <macro.devxpi@gmail.com>
2023-05-19 21:20:58 +0800

    (app): Create a flask app with basic configuration

---
+++ flaskr/__init__.py
@@ -0,0 +1,36 @@
+import os
+from os import environ, path
+from dotenv import load_dotenv
+
+from flask import Flask
+
+BASEDIR = path.abspath(path.dirname(__file__))
+load_dotenv(path.join(BASEDIR, ".env"))
+
+def create_app(test_config=None):
+    # create the flask instance and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY = 'dev',
+        DATABASE = path.join(app.instance_path, 'flaskr.sqlite')
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # a simple page that says hello
+    @app.route('/hello')
+    def hello():
+        return 'Hello, world!'
+    
+    return app
