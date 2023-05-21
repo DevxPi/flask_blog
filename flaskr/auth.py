e88ecf61a3d3e1ddd4e3af2dce4f523ff872ef57
DevxPi <macro.devxpi@gmail.com>
2023-05-21 14:46:59 +0800

    feat(app): update latest auth and blog app

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -1,35 +1,42 @@
 import functools
 
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, session, url_for
+    Blueprint,
+    flash,
+    g,
+    redirect,
+    render_template,
+    request,
+    session,
+    url_for,
 )
 
 from werkzeug.security import check_password_hash, generate_password_hash
 
 from flaskr.db import get_db
 
-bp = Blueprint('auth', __name__, url_prefix='/auth')
+bp = Blueprint("auth", __name__, url_prefix="/auth")
 
 
-@bp.route('/register', methods=['GET', 'POST'])
+@bp.route("/register", methods=["GET", "POST"])
 def register():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
 
         db = get_db()
         error = None
 
         if not username:
-            error = 'Username is required'
+            error = "Username is required"
         elif not password:
-            error = 'Password is required'
+            error = "Password is required"
 
         if error is None:
             try:
                 db.execute(
                     "INSERT  INTO user (username, password) VALUES (?, ?)",
-                    (username, generate_password_hash(password))
+                    (username, generate_password_hash(password)),
                 )
                 db.commit()
             except db.IntegrityError:
@@ -37,65 +44,70 @@ def register():
             else:
                 return redirect(url_for("auth.login"))
         flash(error)
-    
-    return render_template('auth/register.html')
 
-@bp.route('login', methods=['GET','POST'])
+    return render_template("auth/register.html")
+
+
+@bp.route("login", methods=["GET", "POST"])
 def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
 
         db = get_db()
         error = None
 
         user = db.execute(
-            'SELECT * FROM user WHERE username = ?',(username,)
+            "SELECT * FROM user WHERE username = ?", (username,)
         ).fetchone()
 
         if user is None:
-            error = 'Incorrect username'
-        elif not check_password_hash(user['password'], password):
-            error = 'Incorrect password'
-        
+            error = "Incorrect username"
+        elif not check_password_hash(user["password"], password):
+            error = "Incorrect password"
+
         if error is None:
             session.clear()
-            session['user_id'] = user['id']
+            session["user_id"] = user["id"]
 
-            return redirect(url_for('index'))
-        
+            return redirect(url_for("index"))
+
         flash(error)
-    
-    return render_template('auth/login.html')
+
+    return render_template("auth/login.html")
 
 
 @bp.before_app_request
 def load_logged_in_user():
-    user_id = session.get('user_id')
+    user_id = session.get("user_id")
 
     if user_id is None:
         g.user = None
     else:
-        g.user = get_db().connect(
-            'SELECT * FROM user WHERE id = ?',(user_id,)
-        ).fetchone()
+        g.user = (
+            get_db().execute("SELECT * FROM user WHERE id = ?", (user_id,)).fetchone()
+        )
 
-@bp.route('/logout')
+
+@bp.route("/logout")
 def logout():
     session.clear()
-    return redirect(url_for('index'))
+    return redirect(url_for("index"))
 
-# Require Authentication in other views 
+
+# Require Authentication in other views
 def login_required(view):
-    """ Returns a new view function that wraps the original views it's applied too
-        The new function check if a user is loaded and redirects to the login page otherwise.
-        If a user is loaded, the original view is called and continues normally.
-        use this decorator when writing the blog views.
+    """Returns a new view function that wraps the original views it's applied too
+    The new function check if a user is loaded and redirects to the login page otherwise.
+    If a user is loaded, the original view is called and continues normally.
+    use this decorator when writing the blog views.
     """
+
     @functools.wraps(view)
     def wrapped_view(**kwargs):
         if g.user is None:
-            return redirect(url_for('auth.login'))
-        
+            return redirect(url_for("auth.login"))
+
         return view(**kwargs)
+
     return wrapped_view
