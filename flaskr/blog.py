ce7390f4c07c6776327ef281413407e24f35ea6c
DevxPi <macro.devxpi@gmail.com>
2023-05-22 16:34:30 +0800

    feat(app): create like/unlike button for post

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -1,4 +1,13 @@
-from flask import Blueprint, flash, g, redirect, render_template, request, url_for
+from flask import (
+    Blueprint,
+    flash,
+    g,
+    redirect,
+    render_template,
+    request,
+    url_for,
+    session,
+)
 
 from werkzeug.exceptions import abort
 
@@ -69,6 +78,29 @@ def get_post(id, check_author=True):
     return post
 
 
+def get_post_like(id):
+    post_like = (
+        get_db()
+        .execute("SELECT * FROM post_like WHERE post_id = ? AND is_liked = 1", (id,))
+        .fetchall()
+    )
+
+    return post_like
+
+
+def has_post_like(id):
+    has_like = (
+        get_db()
+        .execute(
+            "SELECt * FROM post_like WHERE post_id = ? AND user_id = ? AND is_liked = 1 ",
+            (id, g.user["id"]),
+        )
+        .fetchone()
+    )
+
+    return has_like
+
+
 # update route for update post
 @bp.route("/<int:id>/update", methods=["GET", "POST"])
 @login_required
@@ -94,6 +126,7 @@ def update(id):
             return redirect(url_for("blog.index"))
     return render_template("blog/update.html", post=post)
 
+
 # Delete route for delete post
 @bp.route("/<int:id>/delete", methods=["GET", "POST"])
 @login_required
@@ -108,8 +141,72 @@ def delete(id):
 
 # detail view for showing single post.
 @bp.route("/<int:id>/view")
-@login_required
 def post_single_view(id):
     post = get_post(id, check_author=False)
+    like_post = get_post_like(id)
+    like_post = len(like_post)
 
-    return render_template('blog/view.html', post=post)
+    user_id = session.get("user_id")
+
+    if user_id is not None:
+        user_liked = has_post_like(id)
+    else:
+        user_liked = False
+
+    return render_template(
+        "blog/view.html", post=post, total_like=like_post, user_liked=user_liked
+    )
+
+
+@bp.route("/like/<int:post_id>", methods=["POST"])
+@login_required
+def like_post(post_id):
+    # chck if the user has already liked the post
+    like = (
+        get_db()
+        .execute(
+            "SELECT * FROM post_like WHERE post_id = ? AND user_id=?",
+            (post_id, g.user["id"]),
+        )
+        .fetchone()
+    )
+
+    if like is None:
+        # create a new like entry
+        db = get_db()
+        db.execute(
+            "INSERT INTO post_like (post_id, user_id, is_liked) VALUES (?, ?, ?)",
+            (post_id, g.user["id"], 1),
+        )
+        db.commit()
+    else:
+        db = get_db()
+        db.execute(
+            "UPDATE post_like SET is_liked = 1 WHERE post_id = ? AND user_id = ?",
+            (post_id, g.user["id"]),
+        )
+        db.commit()
+
+    return redirect(url_for("blog.post_single_view", id=post_id))
+
+
+@bp.route("/unlike/<int:post_id>", methods=["POST"])
+@login_required
+def unlike_post(post_id):
+    # chck if the user has already liked the post
+    like = (
+        get_db()
+        .execute(
+            "SELECT * FROM post_like WHERE post_id = ? AND user_id=? AND is_liked = 1",
+            (post_id, g.user["id"]),
+        )
+        .fetchone()
+    )
+
+    if like is not None:
+        # create a new like entry
+        db = get_db()
+        db.execute("UPDATE post_like SET is_liked = ? WHERE id = ?", (0, like[0]))
+        db.commit()
+
+    return redirect(url_for("blog.post_single_view", id=post_id))
